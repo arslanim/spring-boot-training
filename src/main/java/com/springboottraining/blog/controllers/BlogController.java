fd37d4724967641fbc85801583af73c935aff19c
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-02-14 23:57:24 +0400

    added blog controller

---
+++ src/main/java/com/springboottraining/blog/controllers/BlogController.java
@@ -0,0 +1,13 @@
+package com.springboottraining.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+    @GetMapping("/blog")
+    public String blogMain(Model model) {
+        return "blog-main";
+    }
+}
