0e1746e39f4e8a6faaec3b993e42e9e9e1f428b0
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-02-15 00:14:10 +0400

    added controller action

--- src/main/java/com/springboottraining/blog/controllers/MainController.java
+++ src/main/java/com/springboottraining/blog/controllers/MainController.java
@@ -10,8 +10,11 @@ public class MainController {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("title", "Главная страница");
-
         return "home";
     }
 
+    @GetMapping("/about")
+    public String about(Model model) {
+        return "about";
+    }
 }
