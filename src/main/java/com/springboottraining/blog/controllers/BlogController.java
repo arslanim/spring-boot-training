3a01a1ca57a94aa1b624528549990b80d65d30fa
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-02-26 01:35:44 +0400

    updated view controller

--- src/main/java/com/springboottraining/blog/controllers/BlogController.java
+++ src/main/java/com/springboottraining/blog/controllers/BlogController.java
@@ -1,13 +1,22 @@
 package com.springboottraining.blog.controllers;
 
+import com.springboottraining.blog.models.Post;
+import com.springboottraining.blog.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BlogController {
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/blog")
     public String blogMain(Model model) {
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
+
         return "blog-main";
     }
 }
